a6b71b5f3eecbcc5c819dbebd156193093edbb3d
Juansubas <juansubas@gmail.com>
2022-11-20 02:09:12 -0500

    Modules export and import

--- JS-REACT/index.js
+++ JS-REACT/index.js
@@ -1,1 +1,14 @@
-console.log("Hellow World!")
+// Debemos importar la function
+
+// import {add, multiply, active, points, title} from "./add.js"
+
+// console.log(active, points, title)
+
+// console.log(add(10, 20))
+
+// console.log(multiply(10,10))
+
+//Acá al importar por defecto podemos cualqueir nombre
+import addModule from "./add.js"
+
+console.log(addModule)
