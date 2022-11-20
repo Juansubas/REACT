f45cbb83cad07570849d9d5ccc3f264698670a2a
Juansubas <juansubas@gmail.com>
2022-11-19 19:23:52 -0500

    Destructuring

--- JS-REACT/index.js
+++ JS-REACT/index.js
@@ -1,1 +1,18 @@
-console.log("Hello world!!")
+const user = {
+    name: 'Juan',
+    age: 30
+}
+
+// Destructuración
+// al usar { name }
+// lo que hacemos es que extraemos el name
+// y podemos usarlo como variable
+// ahí le pasamos el objeto user de otra forma tocaría hacer lo siguiente
+// const { name } = user
+// poner la variable destructurada y de donde sale
+
+function xd( {name} ) {
+    return name;
+}
+
+console.log(xd(user))
