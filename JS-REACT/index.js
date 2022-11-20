3ab250bcffcc29068917731abe3715809fae8877
Juansubas <juansubas@gmail.com>
2022-11-19 19:33:56 -0500

    Anonymous Functions

--- JS-REACT/index.js
+++ JS-REACT/index.js
@@ -10,9 +10,16 @@ const user = {
 // ahí le pasamos el objeto user de otra forma tocaría hacer lo siguiente
 // const { name } = user
 // poner la variable destructurada y de donde sale
+// podemos cambairle el nombre de la siguiente manera
+// const { name: nombre } = user
+
 
 function xd( {name} ) {
     return name;
+}
+
+function ejemploDos( user ){
+    const { name, age } = user
 }
 
 console.log(xd(user))
