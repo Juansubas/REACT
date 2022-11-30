aa04693908d3708fb81b070475de0dd8351cce2f
Juansubas <juansubas@gmail.com>
2022-11-30 02:09:15 -0500

    Creating File css

--- react-practical/src/Task.js
+++ react-practical/src/Task.js
@@ -1,9 +1,10 @@
+import './task.css';
+
 export function TaskCard() {
-  const cardStyles = { background: "#000000", color: "white", padding: "20px" };
 
   return (
-    <div style={cardStyles}>
-      <h1 style={{ fontWeight: "lighter" }}>Mi primer tarea</h1>
+    <div class='card'>
+      <h1 >Mi primer tarea</h1>
       <p>Tarea Realizada</p>
     </div>
   );
