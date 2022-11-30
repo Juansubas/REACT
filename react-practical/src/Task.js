0b91262b92afabe518307f934db44bba18bdc49d
Juansubas <juansubas@gmail.com>
2022-11-30 02:19:09 -0500

    Ternary ClassName

--- react-practical/src/Task.js
+++ react-practical/src/Task.js
@@ -1,13 +1,12 @@
-import './task.css';
+import "./task.css";
 
-export function TaskCard({ready}) {
-
+export function TaskCard({ ready }) {
   return (
-    <div className='card'>
-      <h1 >Mi primer tarea</h1>
-      <span
-        style={ready ? {background: 'green'} : {background: 'red'}}
-      >{ready ? "Tarea Realizada" : "Tarea pendiente"}</span>
+    <div className="card">
+      <h1>Mi primer tarea</h1>
+      <span className={ready ? "bg-green" : "bg-red"}>
+        {ready ? "Tarea Realizada" : "Tarea pendiente"}
+      </span>
     </div>
   );
 }
