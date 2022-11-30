fd63f430e7a7383d95d5092e8530dfe871cc1944
Juansubas <juansubas@gmail.com>
2022-11-29 19:51:58 -0500

    Module and component

--- react-practical/src/index.js
+++ react-practical/src/index.js
@@ -1,18 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Greeting } from './Greeting';
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-
-function Greeting() {
-
-  function add(x, y) {
-    return x + y
-  }
-
-  return <>
-    <h1> { add(10, 30) }</h1>
-  </>;
-}
 
 root.render(
   <>
