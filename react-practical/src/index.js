8eabb16aceab8a2a16371139ee585774abebbb19
Juansubas <juansubas@gmail.com>
2022-11-29 21:27:22 -0500

    Props exampleDD

--- react-practical/src/index.js
+++ react-practical/src/index.js
@@ -1,16 +1,30 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import {  Greeting, UserCard } from './Greeting';
-import Product, {Navbar} from './Product'
+import { Greeting, UserCard } from "./Greeting";
+import Product, { Navbar } from "./Product";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <>
-    <Greeting title="Hola Mundo" name="Joe"/>
-    <Greeting title="Hola React" lastname="Doe"/>
-    <Greeting title="Hola JSX" />
-    <Greeting title="Hola JavaScript" />
-    <Greeting title="Hola Juan" />
+    <UserCard
+      name="Ryan Ray"
+      amount={3000}
+      married={true}
+      points={[99, 33.3, 22.2]}
+      address={{ street: "123 Main Street", city: "New York" }}
+      greet={function () { alert('Hello')}}
+    />
+
+    <UserCard 
+      name="Joe Mcmillan"
+      amount={1000}
+      married={false}
+      points={[100, 20]}
+      address={{
+        street:"av some 123",
+        city: 'new york'
+      }}
+      />
   </>
 );
