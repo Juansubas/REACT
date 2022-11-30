f165fa535c758e1dbfb2f4f4edd5aedb2be47b77
Juansubas <juansubas@gmail.com>
2022-11-29 22:31:24 -0500

    Inline Css

--- react-practical/src/index.js
+++ react-practical/src/index.js
@@ -3,32 +3,12 @@ import ReactDOM from "react-dom/client";
 import { Greeting, UserCard } from "./Greeting";
 import Product, { Navbar } from "./Product";
 import {Button} from './Button'
+import {TaskCard} from './Task'
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <>
-  <Button text='Click me' />
-  <Button text='Pay' />
-  <Button text='Hello World' name='Joe' />
-    {/* <UserCard
-      name="Ryan Ray"
-      amount={3000}
-      married={true}
-      points={[99, 33.3, 22.2]}
-      address={{ street: "123 Main Street", city: "New York" }}
-      greet={function () { alert('Hello')}}
-    />
-
-    <UserCard 
-      name="Joe Mcmillan"
-      amount={1000}
-      married={false}
-      points={[100, 20]}
-      address={{
-        street:"av some 123",
-        city: 'new york'
-      }}
-      /> */}
+    <TaskCard/>
   </>
 );
