707aa0a5d3a44d7b83624f963d5d7c0c7b6e0861
Juansubas <juansubas@gmail.com>
2022-12-04 14:58:55 -0500

    Creating TaskForm

--- react-tasks-app/src/App.jsx
+++ react-tasks-app/src/App.jsx
@@ -1,9 +1,11 @@
 import TaskList from './TaskList'
+import TaskForm from './TaskForm'
 
 function HolaMundo() {
   return (
     <div>
-      <TaskList/>
+      <TaskForm />
+      <TaskList />
     </div>
   )
 }
