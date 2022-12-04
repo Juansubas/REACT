061799dafca168d566b201e6736d271d7935b2cf
Juansubas <juansubas@gmail.com>
2022-12-03 21:12:05 -0500

    Recorrer arrays map

--- react-practical/src/index.js
+++ react-practical/src/index.js
@@ -2,24 +2,25 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { Post } from "./Posts";
 
-
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-const user = [
+const users = [
   {
     id: 1,
-    name: 'Juan xd',
-    image: 'https://robohash.org/user1'
+    name: "Juan xd",
+    image: "https://robohash.org/user1",
   },
   {
     id: 1,
-    name: 'Joe xd',
-    image: 'https://robohash.org/user2'
-  }
-]
+    name: "Joe xd",
+    image: "https://robohash.org/user2",
+  },
+];
 
 root.render(
   <>
-    <Post/>
+    {users.map((user, i) => {
+      return <h1 key={i}>{user.name}</h1>;
+    })}
   </>
 );
