9b5a3cf1b4738a847e0d6de5cb86e252e2755bad
Juansubas <juansubas@gmail.com>
2022-12-05 12:16:07 -0500

    TaskForm and TaskList Ready

--- react-tasks-app/src/App.jsx
+++ react-tasks-app/src/App.jsx
@@ -1,13 +1,29 @@
 import TaskList from './TaskList'
 import TaskForm from './TaskForm'
+import { tasks as data} from "./tasks";
+import {useState, useEffect} from 'react'
 
-function HolaMundo() {
+function App() {
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    setTasks(data)
+  }, [])
+
+  function createTask(taskTitle) {
+    setTasks([...tasks, {
+      title: taskTitle,
+      id: tasks.length,
+      description: "nueva tarea"
+    }])
+  }
+
   return (
     <div>
-      <TaskForm />
-      <TaskList />
+      <TaskForm createTask={createTask}/>
+      <TaskList tasks={tasks}/>
     </div>
   )
 }
 
-export default HolaMundo
+export default App
