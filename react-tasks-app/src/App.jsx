0b77dfd134ff138cbb51c6b023a59363c195309b
Juansubas <juansubas@gmail.com>
2022-12-05 17:10:03 -0500

    UseContext

--- react-tasks-app/src/App.jsx
+++ react-tasks-app/src/App.jsx
@@ -1,32 +1,13 @@
 import TaskList from './components/TaskList'
 import TaskForm from './components/TaskForm'
-import { tasks as data} from "./data/tasks";
-import {useState, useEffect} from 'react'
 
 function App() {
-  const [tasks, setTasks] = useState([]);
-
-  useEffect(() => {
-    setTasks(data)
-  }, [])
-
-  function createTask(task) {
-    setTasks([...tasks, {
-      title: task.title,
-      id: tasks.length,
-      description: task.description
-    }])
-  }
-
-  function deleteTask(taskId) {
-    setTasks(tasks.filter(task => task.id !== taskId))
-  }
 
   return (
-    <div>
-      <TaskForm createTask={createTask}/>
-      <TaskList tasks={tasks} deleteTask={deleteTask}/>
-    </div>
+    <>
+      <TaskForm />
+      <TaskList />
+    </>
   )
 }
 
