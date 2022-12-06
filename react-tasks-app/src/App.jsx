d42d6e3fb026ee564780e2546b0b50fe7583c3db
Juansubas <juansubas@gmail.com>
2022-12-06 12:04:56 -0500

    TAILWINDCSS

--- react-tasks-app/src/App.jsx
+++ react-tasks-app/src/App.jsx
@@ -1,14 +1,15 @@
-import TaskList from './components/TaskList'
-import TaskForm from './components/TaskForm'
+import TaskList from "./components/TaskList";
+import TaskForm from "./components/TaskForm";
 
 function App() {
-
   return (
-    <>
-      <TaskForm />
-      <TaskList />
-    </>
-  )
+    <main className="bg-zinc-900 h-screen">
+      <div className="cointaner mx-auto p-10">
+        <TaskForm />
+        <TaskList />
+      </div>
+    </main>
+  );
 }
 
-export default App
+export default App;
