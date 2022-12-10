28644a1877d3492e8534b912e5325341601b3481
Juansubas <juansubas@gmail.com>
2022-12-10 01:49:33 -0500

    Link and Params

--- react-router-dom-v6/src/App.js
+++ react-router-dom-v6/src/App.js
@@ -5,6 +5,7 @@ import AboutPage from "./pages/AboutPage";
 import UsersPage from "./pages/UsersPage";
 import NotFoundPage from "./pages/NotFoundPage";
 import Navbar from './components/Navbar'
+import UserPage from "./pages/UserPage";
 
 export default function App() {
   return (
@@ -14,6 +15,7 @@ export default function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="/about" element={<AboutPage />} />
         <Route path="/users" element={<UsersPage />} />
+        <Route path="/users/:id" element={<UserPage />} />
         <Route path="*" element={<NotFoundPage />} />
       </Routes>
     </BrowserRouter>
--- react-router-dom-v6/src/pages/HomePage.js
+++ react-router-dom-v6/src/pages/HomePage.js
@@ -1,5 +1,14 @@
+import {Link} from 'react-router-dom'
+
+const userId = 'fazt'
+
 export default function HomePage() {
   return (
-    <div>HomePage</div>
+    <div>
+      <h1>Applications</h1>
+
+      <Link to={`/users/${userId}`}>Usuarios</Link>
+
+    </div>
   )
 }
