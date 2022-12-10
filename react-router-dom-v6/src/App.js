19f977671b93ee7e087a828880d471577fd1e87b
Juansubas <juansubas@gmail.com>
2022-12-10 01:14:37 -0500

    NavLink

--- react-router-dom-v6/src/App.js
+++ react-router-dom-v6/src/App.js
@@ -4,10 +4,12 @@ import HomePage from "./pages/HomePage";
 import AboutPage from "./pages/AboutPage";
 import UsersPage from "./pages/UsersPage";
 import NotFoundPage from "./pages/NotFoundPage";
+import Navbar from './components/Navbar'
 
 export default function App() {
   return (
     <BrowserRouter>
+      <Navbar/>
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/about" element={<AboutPage />} />
