99199c5b01a0cabd365a31fb22ce93bf1be42eca
Juansubas <juansubas@gmail.com>
2022-12-10 00:38:28 -0500

    Path Not Found

--- react-router-dom-v6/src/App.js
+++ react-router-dom-v6/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import AboutPage from "./pages/AboutPage";
 import UsersPage from "./pages/UsersPage";
+import NotFoundPage from "./pages/NotFoundPage";
 
 export default function App() {
   return (
@@ -11,6 +12,7 @@ export default function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="/about" element={<AboutPage />} />
         <Route path="/users" element={<UsersPage />} />
+        <Route path="*" element={<NotFoundPage />} />
       </Routes>
     </BrowserRouter>
   );
