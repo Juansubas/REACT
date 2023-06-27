06d2bf13d8ceb3246b70cd96652569e60fdbac6b
juansubas <juansubas@gmail.com>
2023-06-27 00:52:10 -0500

    FORM y events y tipos en TS

--- REACT-TYPESCRIPT/Juan-Project/src/App.tsx
+++ REACT-TYPESCRIPT/Juan-Project/src/App.tsx
@@ -1,21 +1,50 @@
-import {useState } from 'react';
+import {MouseEventHandler, useState } from 'react';
 import './App.css'
 
+//Formas de indicar tipos
+
+// (a:string) => void ;  React.Dispatch<React.SetStateAction<string>>
+// HTMLElement => HTMLInputElement
+// string => "name" | "password"
+// React.SyntheticEvent  React.mouseEvent => React.MouseEvent<HtmlDivElement>
+
+//Algunos Eventos
+
+// AnimationEvent => React.AnimationEvent<HTMLDivElement>
+// ChangeEvent => React.ChangeEvent<HTMLInputElement>
+// ClipboardEvent => React.ClipboardEvent<HTMLDivElement>
+// CompositionEvent => React.CompositionEvent<HTMLDivElement>
+// DragEvent => React.DragEvent<HTMLDivElement>
+// FocuEvent => React.FocusEvent<HTMLDivElement>
+// FormEvent => React.FormEvent<HTMLFormElement>
+// KeyboardEvent => React.KeyboardEvent<HTMLDivElement>
+// MouseEvent => React.MouseEvent<HTMLDivElement>
+// PointerEvent => React.PointerEvent<HtmlDivElement>
+// TouchEvent => React.TouchEvent<HTMLDivElement>
+// TransitionEvent => React.TransitionEvent<HTMLDivElement>
+
+// El sintetico es como un any de los eventos
+// SyntheticEvent => React.SyntheticEvent<HTMLDivElement>
 
 function App() {
 
   const [clickPos, setClickPos] = useState({ x: 0, y:0 });
 
-  const handleSubmit = e => {
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    const name = e.currentTarget.elements.namedItem('name');
-    const password = e.target.password;
+
+    const target = e.target as typeof e.target & {
+      name: { value : string};
+      password: { value : string}
+    };
+
+    const name = e.currentTarget.elements.namedItem('name') as HTMLInputElement;
+    const password = target.password;
 
     console.log(name.value, password.value)
   };
 
-  const handleClick = (e:React.MouseEvent<HTMLDivElement, MouseEvent>) =>{
-    const backgroundColor = e.currentTarget.style.backgroundColor;
+  const handleClick: React.MouseEventHandler<HTMLDivElement> = e =>{
     setClickPos({ x: e.clientX, y: e.clientY})
   };
 
