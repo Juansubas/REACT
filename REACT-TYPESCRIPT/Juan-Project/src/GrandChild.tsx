08ee071a70916974ee878426fc25c6410b88835a
juansubas <juansubas@gmail.com>
2023-07-20 14:08:17 -0500

    Try catch for handle errors

--- REACT-TYPESCRIPT/Juan-Project/src/GrandChild.tsx
+++ REACT-TYPESCRIPT/Juan-Project/src/GrandChild.tsx
@@ -5,8 +5,21 @@ type Props = {};
 function GrandChild ({}: Props) {
 
     // ... some code
+    const randomFun = () => {
+        throw new Error("I'm an error");
+    };
 
-    throw new Error("I'm an error");
+    try {
+        randomFun();
+    }catch(error) {
+        if(error instanceof SyntaxError){
+            console.error(error.message);
+        } else if (error instanceof TypeError) {
+            console.error(error.message);
+        } else {
+            //
+        }
+    }
 
     return (
         <div>
