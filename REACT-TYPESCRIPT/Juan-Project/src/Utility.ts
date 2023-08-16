996d17f83b5bfbe496cb19b7259dccb74f15f264
juansubas <juansubas@gmail.com>
2023-08-15 21:09:38 -0500

    Utility

--- REACT-TYPESCRIPT/Juan-Project/src/Utility.ts
+++ REACT-TYPESCRIPT/Juan-Project/src/Utility.ts
@@ -12,10 +12,23 @@ let x : Parameters<typeof devolverNumeroAsync>
 
 type Preferences = string;
 
-const preferences: Preferences = localStorage.getItem('preferences');
+const preferences: Preferences = localStorage.getItem('preferences')!;
 
 //Desaparece cualquier tipo null y asi limpiamos el valor quedando solo string por ejemplo.
 type CleanPreferences = NonNullable<Preferences>
 
 
 const cleanPreferences: CleanPreferences = preferences;
+
+
+interface PersonaInmutable {
+    name: string;
+    age: number;
+}
+
+const juanitoInmutable: Readonly<PersonaInmutable> = {
+    name: 'Juan',
+    age: 20,
+}
+
+//juanitoInmutable.age = 21;
