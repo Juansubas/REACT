0e23ef3b81a7c01a81a9fd258be75857a95ae25f
juansubas <juansubas@gmail.com>
2023-08-22 06:08:24 -0500

    Higher order Components

--- REACT-TYPESCRIPT/Juan-Project/src/App.tsx
+++ REACT-TYPESCRIPT/Juan-Project/src/App.tsx
@@ -1,21 +1,68 @@
 import './App.css'
-import Polymorphic from './Polymorphic';
 
-//TypeSafety
 function App() {
 
   return(
     <div > 
-      <Polymorphic
-      as="div" 
-      href="https://www.google.com" 
-      target= "_blank"
-      variant="black"
-      >
-        Hola
-      </Polymorphic>
+      <SubsribedSideBar name={name} />
+      <SubsribedHeader name={name} />
     </div>
   );
 }
+
+type OriginalProps = {
+  name: string;
+};
+
+const SideBar = ({ name} :OriginalProps) => {
+  // ... a lot code
+  return <div> Helo, {name}</div>;
+}
+
+const Header = ({ name }: OriginalProps) => {
+  // ... a lot code
+  const favs = [1, 2, 3, 4, 5];
+  return (
+      <div>
+        Your favorites, {name}
+        {favs.map(fav => (
+          <div>{fav}</div>
+        ))}
+      </div>
+  );
+};
+
+
+type OriginalProps2 = {
+  name: string;
+  date: string;
+};
+
+const Nav = ({name} :OriginalProps2) =>{
+  // ... a lot code.
+  return <div> Hola, {name}</div>;
+};
+
+type ExtendedProps =  OriginalProps & {
+  isSubscribed: boolean;
+};
+
+function withSubscribed(OriginalComponent : React.ComponentType<OriginalProps>) {
+
+  const WithSubscribed = (props:ExtendedProps) => {
+    
+    const {isSubscribed} = props;
+
+    if(!isSubscribed) return <div>Subscribe please</div>
+
+    return <OriginalComponent {...props}/>
+  }
+
+  return WithSubscribed;
+}
+
+const SubsribedSideBar = withSubscribed(SideBar);
+const SubsribedHeader = withSubscribed(Header);
+
 
 export default App;
