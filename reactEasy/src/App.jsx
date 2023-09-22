d08459cade212e6b9b255af44eaaab2dfa9f140e
juansubas <juansubas@gmail.com>
2023-09-21 22:39:41 -0500

    Props con objetos, prop children y etc

--- reactEasy/src/App.jsx
+++ reactEasy/src/App.jsx
@@ -2,33 +2,17 @@ import "./App.css";
 import { TwitterFollowCard } from "./TwitterFollowCard";
 
 export function App() {
-  const formatUserName = (userName) => `@${userName}`;
   return (
-    <div className="App">
-      <TwitterFollowCard
-        formatUserName={formatUserName}
-        isFollowing
-        userName="midudev"
-        name="Miguel Angel Duran"
-      />
-      <TwitterFollowCard
-        formatUserName={formatUserName}
-        isFollowing={false}
-        userName="pheralb"
-        name="Pablo Hernandez"
-      />
-      <TwitterFollowCard
-        formatUserName={formatUserName}
-        isFollowing
-        userName="elonmusk"
-        name="Elon Musk"
-      />
-      <TwitterFollowCard
-        formatUserName={formatUserName}
-        isFollowing
-        userName="vxnder"
-        name="Vanderhart"
-      />
-    </div>
+    <section className="App">
+      
+      <TwitterFollowCard isFollowing userName="midudev">
+        Miguel Angel Duran
+      </TwitterFollowCard>
+
+      <TwitterFollowCard isFollowing={false} userName="pheralb">      
+        Pablo Hernandez
+      </TwitterFollowCard>
+
+    </section>
   );
 }
--- reactEasy/src/TwitterFollowCard.jsx
+++ reactEasy/src/TwitterFollowCard.jsx
@@ -1,22 +1,22 @@
-export function TwitterFollowCard ( { formatUserName, userName, name, isFollowing}){
-    const imageSrc = `https://unavatar.io/${userName}`
+export function TwitterFollowCard({ children,userName, name, isFollowing }) {
+  const imageSrc = `https://unavatar.io/${userName}`;
 
-    return (
-        <article className="tw-followCard">
-        <header className="tw-followCard-Header">
-          <img
-            className="tw-followCard-avatar"
-            src={imageSrc}
-            alt="El avatar de Midudev"
-          />
-          <div className="tw-followCard-info">
-            <strong>{name}</strong>
-            <span className="tw-followCard-infoUserName">{formatUserName(userName)}</span>
-          </div>
-        </header>
-        <aside>
-          <button className="tw-followCard-button">Seguir</button>
-        </aside>
-      </article>
-    );
+  return (
+    <article className="tw-followCard">
+      <header className="tw-followCard-Header">
+        <img
+          className="tw-followCard-avatar"
+          src={imageSrc}
+          alt="El avatar de Midudev"
+        />
+        <div className="tw-followCard-info">
+          <strong>{children}</strong>
+          <span className="tw-followCard-infoUserName">@{userName}</span>
+        </div>
+      </header>
+      <aside>
+        <button className="tw-followCard-button">Seguir</button>
+      </aside>
+    </article>
+  );
 }
