4bd765bb0b08a8a5d80f828d17cd2a50d6d6e81b
juansubas <juansubas@gmail.com>
2023-10-07 03:23:03 -0500

    Proyect MousePointer

--- Learn React/03-mouse-follower/src/App.jsx
+++ Learn React/03-mouse-follower/src/App.jsx
@@ -1,6 +1,52 @@
+import { useEffect, useState } from "react"
+
 function App() {
+  const [enabled, setEnabled] = useState(false)
+  const [position, setPosition] = useState({ x: 0, y: 0 })
+
+  useEffect(()=>{
+    console.log('efecto')
+
+    const handleMove = (event) => {
+      console.log(event)
+      const { clientX, clientY } = event
+      console.log('handleMove', { clientX, clientY})
+      setPosition( { x: clientX, y: clientY } )
+    }
+
+    if (enabled) {
+      window.addEventListener('pointermove', handleMove)
+    }
+
+    // cleanup 
+    // cuando el componente se desmonta 
+    // cambia las dependencias antes de ejecutar 
+    // el efecto de nuevo 
+    return () => {
+      window.removeEventListener('pointermove', handleMove)
+    }
+  }, [enabled])
+
   return (
-    <h3>Proyecto 3</h3>
+    <main>
+      <div style={{
+        position: 'absolute',
+        backgroundColor: '#09f',
+        borderRadius: '50%',
+        opacity: 0.8,
+        pointerEvents: 'none',
+        left: -20,
+        top: -20,
+        width: 40,
+        height: 40,
+        transform: `translate(${position.x}px, ${position.y}px)`
+        }}
+      >
+      </div>
+      <button onClick={() => setEnabled(!enabled)}>
+        {enabled ? 'Desactivar' : 'Activar'} seguir puntero
+      </button>
+    </main>
   )
 }
 
