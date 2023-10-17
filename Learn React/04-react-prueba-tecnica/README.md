10c4390bbad90a0d00f11181cb02c21f3de1a320
juansubas <juansubas@gmail.com>
2023-10-17 05:11:23 -0500

    base terminado

---
+++ Learn React/04-react-prueba-tecnica/README.md
@@ -0,0 +1,11 @@
+Enunciado Prueba Técnica:
+
+# Prueba técnica para Juniors y Trainees de React en Live Coding
+
+APIs:
+
+- Facts Random: https://catfact.ninja/fact
+- Imagen Random: https://cataas.com/cat/says/hello
+
+Recupera un hecho aleatorio de gatos y muestra una imagen de un pato con la primera palabra del hecho
+recuperado usando la segunda API.
---
+++ Learn React/04-react-prueba-tecnica/src/App.jsx
@@ -0,0 +1,31 @@
+import { useEffect, useState } from "react"
+
+const CAT_ENDPOINT_RANDOM_FACT = 'https://catfact.ninja/fact'
+const CAT_PREFIX_IMAGE_URL = 'https://cataas.com/cat/says/'
+export function App () {
+
+    const [fact, setFact] = useState('lorem ipsum cat fact whatever')
+    const [imageUrl, setImageUrl] = useState()
+
+    useEffect(() => {
+        fetch(CAT_ENDPOINT_RANDOM_FACT)
+        .then(res => res.json())
+        .then(data => {
+            const { fact } = data
+            setFact(fact)
+
+            const firstWord = fact.split(' ', 1)
+            const url = `${CAT_PREFIX_IMAGE_URL}${firstWord}` 
+
+            setImageUrl(url)
+        })
+    }, [])
+
+    return (
+        <main>
+            <h1> App de Gatitos</h1>
+            {fact && <p>{fact}</p>}
+            {imageUrl && <img src={imageUrl} alt={`Image extracted using the first three words for ${fact}`} />}
+        </main>
+    )
+}
