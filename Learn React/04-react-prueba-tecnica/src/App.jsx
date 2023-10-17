91e9c9fdb21827ac96b5d3fe3bb95136f6de3f8f
juansubas <juansubas@gmail.com>
2023-10-17 06:34:53 -0500

    Usando Grid

--- Learn React/04-react-prueba-tecnica/src/App.jsx
+++ Learn React/04-react-prueba-tecnica/src/App.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react"
+import './App.css'
 
 const CAT_ENDPOINT_RANDOM_FACT = 'https://catfact.ninja/fact'
 const CAT_PREFIX_IMAGE_URL = 'https://cataas.com/cat/says/'
@@ -16,16 +17,18 @@ export function App () {
 
             const firstWord = fact.split(' ', 1)
             const url = `${CAT_PREFIX_IMAGE_URL}${firstWord}` 
-
+            console.log(url)
             setImageUrl(url)
         })
     }, [])
 
     return (
-        <main style={{display: 'flex', flexDirection: 'column', placeItems: 'center'}}>
+        <main >
             <h1> App de Gatitos</h1>
-            {fact && <p>{fact}</p>}
-            {imageUrl && <img src={imageUrl} alt={`Image extracted using the first three words for ${fact}`} />}
+            <section>
+                {fact && <p>{fact}</p>}
+                {imageUrl && <img src={imageUrl} alt={`Image extracted using the first three words for ${fact}`} />}
+            </section>
         </main>
     )
 }
