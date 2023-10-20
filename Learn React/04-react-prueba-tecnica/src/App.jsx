75c171e8c04c8a83f3034eba84dc14e18424ec06
juansubas <juansubas@gmail.com>
2023-10-20 01:55:13 -0500

    Separando la logica con Services en react

--- Learn React/04-react-prueba-tecnica/src/App.jsx
+++ Learn React/04-react-prueba-tecnica/src/App.jsx
@@ -1,30 +1,35 @@
 import { useEffect, useState } from "react"
 import './App.css'
+import { getRandomFact } from "./Services/facts"
 
-const CAT_ENDPOINT_RANDOM_FACT = 'https://catfact.ninja/fact'
 const CAT_PREFIX_IMAGE_URL = 'https://cataas.com/cat/says/'
 export function App () {
 
-    const [fact, setFact] = useState('lorem ipsum cat fact whatever')
+    const [fact, setFact] = useState()
     const [imageUrl, setImageUrl] = useState()
 
     useEffect(() => {
-        fetch(CAT_ENDPOINT_RANDOM_FACT)
-        .then(res => res.json())
-        .then(data => {
-            const { fact } = data
-            setFact(fact)
-
-            const firstWord = fact.split(' ', 1)
-            const url = `${CAT_PREFIX_IMAGE_URL}${firstWord}` 
-            console.log(url)
-            setImageUrl(url)
-        })
+        getRandomFact().then(setFact)
     }, [])
+
+    useEffect(() => {
+        if(!fact) return
+
+        const firstWord = fact.split(' ', 1)
+        const url = `${CAT_PREFIX_IMAGE_URL}${firstWord}` 
+        setImageUrl(url)
+        console.log(url)
+    }, [fact])
+
+    const handleClick = async () => {
+        const newFact = await getRandomFact()
+        setFact(newFact)
+    }
 
     return (
         <main >
             <h1> App de Gatitos</h1>
+            <button onClick={handleClick}>Get new fact</button>
             <section>
                 {fact && <p>{fact}</p>}
                 {imageUrl && <img src={imageUrl} alt={`Image extracted using the first three words for ${fact}`} />}
