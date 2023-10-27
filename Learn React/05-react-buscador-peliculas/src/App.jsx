6807d3c622ebd87306f575953af73409d7691424
juansubas <juansubas@gmail.com>
2023-10-26 19:12:19 -0500

    Encapsulando la logica y el estado en un useHook

--- Learn React/05-react-buscador-peliculas/src/App.jsx
+++ Learn React/05-react-buscador-peliculas/src/App.jsx
@@ -1,20 +1,65 @@
 import './App.css'
 import { Movies } from './Components/Movies'
 import { useMovies } from './hooks/useMovies'
+import { useEffect, useState } from 'react'
 
+function useSearch() {
+  const [search, updateSearch] = useState('')
+  const [error, setError] = useState(null)
+
+  useEffect( () => {
+    if (search === '') {
+      setError('No se puede buscar una pelicula vacia')
+      return
+    }
+
+    if (search.match(/^\d+$/)) {
+      setError('No se puede buscar una pelicula con un numero')
+      return
+    }
+
+    if (search.length < 3) {
+      setError('La busqueda debe tener al menos 3 caracteres')
+      return
+    }
+
+    setError(null)
+  }
+  , [search])
+
+  return { search, updateSearch, error}
+}
 
 function App() {
   const { movies: mappedMovies } = useMovies()
+  const { search, updateSearch, error} = useSearch()
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    console.log({ search })
+  }
+
+  const handleChange = (event) => {
+    updateSearch(event.target.value)
+  }
 
   return (
     <div className='page'>
 
       <header>
         <h1>Buscador de Peliculas</h1>
-        <form className='form'>
-          <input placeholder='Avengers, Star Wars, The Matrix...' type='text' />
+        <form className='form' onSubmit={handleSubmit}>
+          <input 
+          style={{
+            border: '1px solid transparent',
+            borderColor: error ? 'red' : 'transparent'
+          }}
+          value={search} onChange={handleChange} placeholder='Avengers, Star Wars, The Matrix...' type='text' />
           <button type='submit'>Buscar</button>
         </form>
+        {error && <p style={{ color: 'red'}}>
+          {error}
+        </p> }
       </header>
 
       <main>
