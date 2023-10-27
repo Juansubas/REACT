1567f093de3b3e8d9d3175ab7217e8fc1c5ec616
juansubas <juansubas@gmail.com>
2023-10-26 20:33:44 -0500

    Api funcionando con fetch

--- Learn React/05-react-buscador-peliculas/src/App.jsx
+++ Learn React/05-react-buscador-peliculas/src/App.jsx
@@ -37,12 +37,12 @@ function useSearch() {
 }
 
 function App() {
-  const { movies: mappedMovies } = useMovies()
   const { search, updateSearch, error} = useSearch()
+  const { movies: mappedMovies, getMovies } = useMovies({ search })
 
   const handleSubmit = (event) => {
     event.preventDefault()
-    console.log({ search })
+    getMovies()
   }
 
   const handleChange = (event) => {
--- Learn React/05-react-buscador-peliculas/src/hooks/useMovies.js
+++ Learn React/05-react-buscador-peliculas/src/hooks/useMovies.js
@@ -1,8 +1,12 @@
-import responseMovies from '../mocks/with-results.json'
-//import withoutResults from '../mocks/no-results.json'
+import withResults from '../mocks/with-results.json'
+import withoutResults from '../mocks/no-results.json'
+import { useState } from 'react'
 
-export function useMovies () {
+export function useMovies ({ search }) {
+    const [responseMovies, setResponseMovies] = useState([])
+
     const movies = responseMovies.Search
+
     const mappedMovies = movies?.map(movie => ({
       id : movie.imdbID,
       title: movie.Title, 
@@ -10,7 +14,20 @@ export function useMovies () {
       poster: movie.Poster
     }
     ))
+
+    const getMovies = () => {
+      if(search) {
+        //setResponseMovies(withResults)
+        fetch(`https://www.omdbapi.com/?apikey=4287ad07&s=${search}`)
+          .then(data => data.json())
+          .then(json => {
+            setResponseMovies(json)
+          })
+      } else {
+        setResponseMovies(withoutResults)
+      }
+    }
   
-    return { movies: mappedMovies}
+    return { movies: mappedMovies, getMovies}
   }
   
