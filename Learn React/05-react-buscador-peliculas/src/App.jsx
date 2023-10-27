d4c7aa88933a070ec82f07749447afc3f1987915
juansubas <juansubas@gmail.com>
2023-10-26 19:28:44 -0500

    usando useRef con bandera para detectar la primera vez

--- Learn React/05-react-buscador-peliculas/src/App.jsx
+++ Learn React/05-react-buscador-peliculas/src/App.jsx
@@ -1,13 +1,19 @@
 import './App.css'
 import { Movies } from './Components/Movies'
 import { useMovies } from './hooks/useMovies'
-import { useEffect, useState } from 'react'
+import { useEffect, useState, useRef } from 'react'
 
 function useSearch() {
   const [search, updateSearch] = useState('')
   const [error, setError] = useState(null)
+  const isFirstInput = useRef(true)
 
   useEffect( () => {
+    if (isFirstInput.current) {
+      isFirstInput.current = search === ''
+      return
+    }
+
     if (search === '') {
       setError('No se puede buscar una pelicula vacia')
       return
