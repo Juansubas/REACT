687270b5f120ac8bb56861386db8731ec3836911
juansubas <juansubas@gmail.com>
2023-11-15 01:31:03 -0500

    Mi primer Contexto y Estado Global

--- Learn React/06-shopping-cart/src/App.jsx
+++ Learn React/06-shopping-cart/src/App.jsx
@@ -1,16 +1,21 @@
 import { products as initialProducts } from './mocks/products.json'
 import { Products } from "./components/Products"
-import { useState } from 'react'
+import { useContext, useState } from 'react'
 import { Header } from './components/Header'
 import { Footer } from './components/Footer'
 import { IS_DEVELOPMENT } from './config'
+import { FiltersContext } from './context/filters'
 
 function useFilters(){
 
-  const [filters, setFilters] = useState({
-    category: 'all',
-    minPrice: 0
-  })
+  // const [filters, setFilters] = useState({
+  //   category: 'all',
+  //   minPrice: 0
+  // })
+
+  //Paso 3 consumir el contexto
+  const { filters, setFilters} = useContext(FiltersContext)
+
 
   const filterProducts = (products) => {
     return products.filter(product => {
