e4000abe66596bed43d814bcf5730f334b307b7c
juansubas <juansubas@gmail.com>
2023-11-15 01:53:36 -0500

    Teneindo contexto y separando la logica del useFilters en un customHook

--- Learn React/06-shopping-cart/src/App.jsx
+++ Learn React/06-shopping-cart/src/App.jsx
@@ -1,35 +1,10 @@
 import { products as initialProducts } from './mocks/products.json'
 import { Products } from "./components/Products"
-import { useContext, useState } from 'react'
+import { useState } from 'react'
 import { Header } from './components/Header'
 import { Footer } from './components/Footer'
 import { IS_DEVELOPMENT } from './config'
-import { FiltersContext } from './context/filters'
-
-function useFilters(){
-
-  // const [filters, setFilters] = useState({
-  //   category: 'all',
-  //   minPrice: 0
-  // })
-
-  //Paso 3 consumir el contexto
-  const { filters, setFilters} = useContext(FiltersContext)
-
-
-  const filterProducts = (products) => {
-    return products.filter(product => {
-      return (
-          product.price >= filters.minPrice && 
-        (
-          filters.category === 'all' ||
-          product.category === filters.category
-        )
-      )
-    })
-  }
-  return { filters, filterProducts, setFilters }
-}
+import { useFilters } from './hooks/useFilters'
 
 function App () {
   const [products] =  useState(initialProducts)
