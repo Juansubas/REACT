1f3890140663e9cd3d9aafd9e8612bbab067ea43
juansubas <juansubas@gmail.com>
2024-08-16 03:15:14 -0500

    Component props

--- React-again/01-TwitterFollowCard/src/App.jsx
+++ React-again/01-TwitterFollowCard/src/App.jsx
@@ -5,12 +5,32 @@ function App() {
   const formatUserName = (userName) => `@${userName}`
 
   return (
-    <>
-      <TwitterFollowCard formatUserName={formatUserName} isFollowing name='Miguel Angel Duran' userName='midudev'/> 
-      <TwitterFollowCard formatUserName={formatUserName} isFollowing={false} name='Pablo Hernandez' userName='pheralb'/> 
-      <TwitterFollowCard formatUserName={formatUserName} isFollowing name='Elon Musk' userName='elonmusk'/> 
-      <TwitterFollowCard formatUserName={formatUserName} isFollowing name='vxnder' userName='VanderHeart'/> 
-    </>
+    <div className='app'>
+      <TwitterFollowCard 
+        formatUserName={formatUserName} 
+        isFollowing 
+        name='Miguel Angel Duran' 
+        userName='midudev'
+      /> 
+      <TwitterFollowCard 
+        formatUserName={formatUserName} 
+        isFollowing={false} 
+        name='Pablo Hernandez' 
+        userName='pheralb'
+      /> 
+      <TwitterFollowCard 
+        formatUserName={formatUserName} 
+        isFollowing 
+        name='Elon Musk' 
+        userName='elonmusk'
+      /> 
+      <TwitterFollowCard 
+        formatUserName={formatUserName} 
+        isFollowing 
+        name='vxnder' 
+        userName='VanderHeart'
+      /> 
+    </div>
   )
 }
 
