5344e28e4b399c0b0f438c7f3c73c7bbf2882747
juansubas <juansubas@gmail.com>
2024-08-16 02:43:56 -0500

    Create coMPONENT

--- React-again/01-TwitterFollowCard/src/App.jsx
+++ React-again/01-TwitterFollowCard/src/App.jsx
@@ -7,28 +7,23 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+    <article className='tw-followCard'>
+      <header className='tw-followCard-header'>
+        <img 
+          src="https://unavatar.io/midudev" 
+          alt="El avatar de Midudev" 
+        />
+        <div className='tw-followCard-info'>
+          <strong>Miguel Angel Duran</strong>
+          <span className='tw-followCard-infoUserName'>@Midudev</span>
+        </div>
+      </header>
+      <aside>
+        <button className='tw-followCard-button'>
+          Seguir
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      </aside>
+    </article>
   )
 }
 
