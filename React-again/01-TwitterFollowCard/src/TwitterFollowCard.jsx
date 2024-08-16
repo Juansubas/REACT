52a8e1603c323e432c8da4a139af67988a61cc24
juansubas <juansubas@gmail.com>
2024-08-16 04:40:16 -0500

    Full

--- React-again/01-TwitterFollowCard/src/TwitterFollowCard.jsx
+++ React-again/01-TwitterFollowCard/src/TwitterFollowCard.jsx
@@ -30,7 +30,8 @@ export const TwitterFollowCard = ({ children, userName, initialIsFollowing}) => 
         </header>
         <aside>
           <button className={buttonClassName} onClick={handleClick}>
-            {text}
+            <span className='tw-followCard-text'>{text}</span>
+            <span className='tw-followCard-stopFollow'>Dejar de Seguir</span>
           </button>
         </aside>
       </article>
