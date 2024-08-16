e6df5997f76a51384af1761e0c6ff2ce282485aa
juansubas <juansubas@gmail.com>
2024-08-16 02:26:10 -0500

    Component Button

--- React-again/01-TwitterFollowCard/src/main.jsx
+++ React-again/01-TwitterFollowCard/src/main.jsx
@@ -4,7 +4,7 @@ import './index.css'
 
 const app = ReactDOM.createRoot(document.getElementById('root'));
 
-const createButton = ({text}) => {
+const Button = ({text}) => {
   return (
     <button>
       <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
@@ -17,8 +17,8 @@ const createButton = ({text}) => {
 
 app.render(
   <React.Fragment>
-    {createButton({ text: 'Button 1'})}
-    {createButton({ text: 'Button 2'})}
-    {createButton({ text: 'Button 3'})}
+    <Button text= "Button 1"></Button>
+    <Button text= "Button 1"></Button>
+    <Button text= "Button 1"></Button>
   </React.Fragment>
-);
+); 
